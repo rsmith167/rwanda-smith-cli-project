c5709336a84b49c6c9277de6251da2213d109a46
rsmith167 <rwanda.ace@gmail.com>
2020-04-20 07:58:40 -0400

    change 7.58

--- lib/rwanda/smith/cli/project/choseaffiliation.rb
+++ lib/rwanda/smith/cli/project/choseaffiliation.rb
@@ -4,5 +4,27 @@ class ChoseAffiliation
     #    ws = ws 
     #     puts "The affiliations are #{ws.afiiliation_clean}"
     # end
-
+    def start
+        @ws = WebsiteScraper
+        puts "The roles are #{@ws.roles_list}"
+        puts "Press    #{@ws.roles_list[0][0]} - #{@ws.roles_list[0]}    #{@ws.roles_list[1][0]} - #{@ws.roles_list[1]}    #{@ws.roles_list[2][0]} - #{@ws.roles_list[2]}   
+        \n\t\tM - Main Menu    H - Help"
+        uichoseroles = ""
+        uichoseroles = gets.chomp
+        uichoseroles.upcase!
+        if uichoseroles  == "T"
+            choosebasedonrole("Tank")  
+        elsif uichoseroles  == "D"
+            choosebasedonrole("Damage")  
+        elsif uichoseroles  == "S"
+            choosebasedonrole("Support")  
+        elsif uichoseroles == "M"
+            StartApp.mainmenu
+        elsif uichoseroles  == "H"
+            StartApp.helpmenu
+        else
+            puts "\nThis is not a valid option"
+            start
+        end
+    end
 end
--- lib/rwanda/smith/cli/project/version.rb
+++ lib/rwanda/smith/cli/project/version.rb
@@ -4,6 +4,8 @@ module Rwanda
       module Project
         VERSION = "0.1.0"
        #test change 4.20.20
+       # awwwwwwwwwwwww yeaaah bois we in it now. I figured out this git pull git push thing. hoh boy! I'm practically a software engineer!
+      StartApp
       end
     end
   end
